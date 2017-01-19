776fdf00067c15d5d6ad4a15504d659741a7d01b
Brandon Soto <brandon.soto09@gmail.com>
2017-01-18 19:37:28 -0800

    Fix formatting

--- chapters/Chapter1-Arrays-Strings/main.cpp
+++ chapters/Chapter1-Arrays-Strings/main.cpp
@@ -33,27 +33,26 @@ namespace ch1 {
      * 2: Check Permutation: Given 2 strings, write a method to decide if one is a permutation of the other.
      * // TODO: can I modify? What's the char set? Does whitespace matter? Does letter case matter?
      ******************************************************************************************************************/
-    bool is_permutation(const std::string &str1, const std::string& str2 ) {
+    bool is_permutation(const std::string &str1, const std::string &str2) {
         // solution 1: sort both strings and compare each char in both O(n log n) - use if I can modify string
         // * solution 2: use a map to count char occurrences O(n)
         // solution 3: linear search and try to find each char in the other string O(n ^ n)
 
         // assuming whitespace and letter case matter
 
-        if ( str1.size() != str2.size() )
+        if (str1.size() != str2.size())
             return false;
 
         std::unordered_map<char, int> char_count{};
-        for ( std::size_t i = 0; i < str1.size(); ++i ) {
+        for (std::size_t i = 0; i < str1.size(); ++i) {
             ++char_count[str1[i]];
             --char_count[str2[i]];
         }
 
-        for ( const auto& pair : char_count ) {
-            if ( pair.second != 0 )
+        for (const auto &pair : char_count) {
+            if (pair.second != 0)
                 return false;
         }
-
 
         return true;
     }
@@ -73,12 +72,12 @@ int main() {
     // test cases for question 2
     std::cout << "is permutation" << std::endl << std::endl;
 
-    std::vector<std::string> perm_test_cases{"", "s", "sa", "sassy", "alym", "a ", " h", "hello"};
+    std::vector<std::string> perm_test_cases1{"", "s", "sa", "sassy", "alym", "a ", " h", "hello"};
     std::vector<std::string> perm_test_cases2{"", "s", "as", "yssas", "myla", " a", "j ", "hello "};
-    for ( std::size_t i = 0; i < perm_test_cases.size(); ++i ) {
-        const auto str1 = perm_test_cases[i];
+    for (std::size_t i = 0; i < perm_test_cases1.size(); ++i) {
+        const auto str1 = perm_test_cases1[i];
         const auto str2 = perm_test_cases2[i];
-        std::cout << str1 << ", " << str2 << " are perms = " <<  std::boolalpha << ch1::is_permutation( str1, str2 ) << std::endl;
+        std::cout << str1 << ", " << str2 << " are perms = " << std::boolalpha << ch1::is_permutation(str1, str2) << std::endl;
     }
 
     return 0;
