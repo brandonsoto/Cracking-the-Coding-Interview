7f46c575af5ca9e9012343760771d47eaabc797d
Brandon Soto <brandon.soto09@gmail.com>
2017-01-22 00:15:47 -0800

    Remove is_palindrome()

--- chapters/Chapter1-Arrays-Strings/answers.hpp
+++ chapters/Chapter1-Arrays-Strings/answers.hpp
@@ -95,19 +95,6 @@ namespace ch1 {
         }
     }
 
-    bool is_palindrome(const std::string &str) {
-        // solution 1: iterate through half of string; compare chars on left and right O(n)
-        const auto size = str.size();
-        const std::size_t end_index = (size % 2 == 0) ? (size / 2 - 1) : (size / 2);
-
-        for (std::size_t start = 0, end = str.size() - 1; start < end_index; ++start, --end) {
-            if (str[start] != str[end])
-                return false;
-        }
-
-        return true;
-    }
-
     /*******************************************************************************************************************
      * 4: Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome.
      * A permutation does not need to be limited to just dictionary words.
