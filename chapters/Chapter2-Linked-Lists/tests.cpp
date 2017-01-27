20f062c50769018f4112b41704da8edd3eb6a2c0
Brandon Soto <brandon.soto09@gmail.com>
2017-01-26 20:25:05 -0800

    Rename create_list to create_linked_list

--- chapters/Chapter2-Linked-Lists/tests.cpp
+++ chapters/Chapter2-Linked-Lists/tests.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <string>
 #include <gtest/gtest.h>
 #include "answers.hpp"
 
 using namespace ch2;
 
-std::shared_ptr<Node> create_list(const std::initializer_list<int> &list) {
+std::shared_ptr<Node> create_linked_list(const std::initializer_list<int> &list) {
     if (list.size() == 0)
         return std::unique_ptr<Node>(nullptr);
 
@@ -27,28 +26,28 @@ void print(const Node &root) {
 }
 
 TEST(Chapter2, Question1) {
-    auto empty_list = create_list({});
-    auto expected = create_list({});
+    auto empty_list = create_linked_list({});
+    auto expected = create_linked_list({});
     remove_duplicates(*empty_list.get());
     EXPECT_EQ(*expected, *empty_list);
 
-    auto size1_list = create_list({1});
-    expected = create_list({1});
+    auto size1_list = create_linked_list({1});
+    expected = create_linked_list({1});
     remove_duplicates(*size1_list.get());
     EXPECT_EQ(*expected, *size1_list);
 
-    auto all_duplicates = create_list({1, 1, 1, 1, 1, 1});
-    expected = create_list({1});
+    auto all_duplicates = create_linked_list({1, 1, 1, 1, 1, 1});
+    expected = create_linked_list({1});
     remove_duplicates(*all_duplicates.get());
     EXPECT_EQ(*expected, *all_duplicates);
 
-    auto no_duplicates = create_list({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
-    expected = create_list({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    auto no_duplicates = create_linked_list({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    expected = create_linked_list({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
     remove_duplicates(*no_duplicates.get());
     EXPECT_EQ(*expected, *no_duplicates);
 
-    auto some_duplicates = create_list({4,1,3,4,1,4});
-    expected = create_list({4,1,3});
+    auto some_duplicates = create_linked_list({4, 1, 3, 4, 1, 4});
+    expected = create_linked_list({4, 1, 3});
     remove_duplicates(*some_duplicates.get());
     EXPECT_EQ(*expected, *some_duplicates);
 }
